5810e17305b338cbc2d5ccfef6d05520f9640d7e
Madis21 <ssmirnoff21@gmail.com>
2023-10-25 11:30:19 +0500

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,5 +1,16 @@
 package org.example;
 
-public class PhoneBook{
+import java.util.ArrayList;
+import java.util.List;
 
+public class PhoneBook {
+    private List<Integer> list;
+
+    public PhoneBook() {
+        list = new ArrayList<>();
+    }
+
+    public int addNumber(int number) {
+        return 0;
+    }
 }
--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,5 +1,15 @@
 package org.example;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PhoneBookTest {
 
+    @Test
+    public void addTest() {
+
+        PhoneBook book = new PhoneBook();
+        int addedNumber = book.addNumber(1234567);
+        Assert.assertEquals(1234567, addedNumber);
+    }
 }
