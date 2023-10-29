e141bbfa4d4290230328237bd253bb0a4309c79a
Madis21 <ssmirnoff21@gmail.com>
2023-10-29 22:25:44 +0500

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,6 +1,17 @@
 package org.example;
 
+import java.util.Map;
+import java.util.TreeMap;
 
 public class PhoneBook {
+    private Map<String, Integer> map;
 
+    public PhoneBook() {
+        map = new TreeMap<>();
+    }
+
+    public int add(String name, int number) {
+        map.put(name, number);
+            return 0;
+    }
 }
--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,5 +1,14 @@
 package org.example;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PhoneBookTest {
 
+    @Test
+    public void addTest() {
+        org.example.PhoneBook book = new org.example.PhoneBook();
+        int size = book.add("Ivan", 1234567);
+        Assert.assertEquals(1, size);
+    }
 }
